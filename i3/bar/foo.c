ae83cf82f58de62ede8f86d2bfcd4d83b4856a97
oberien <jaro.fietz@gmx.de>
2018-01-24 10:57:50 +0100

    xbacklight + mode none

--- i3/bar/foo.c
+++ i3/bar/foo.c
@@ -4,7 +4,7 @@
 #include<fcntl.h>
 
 int main() {
-  int fd = open("/proc/meminfo", O_RDONLY);
+  int fd = open("/sys/class/power_supply/BAT0/status", O_RDONLY);
   int buf[100];
   read(fd, buf, 100);
   buf[99] = 0;
@@ -12,9 +12,11 @@ int main() {
   struct pollfd fds;
   fds.fd = fd;
   fds.events = POLLPRI;
+  lseek(fd, 0, SEEK_SET);
   while (1) {
     poll(&fds, 1, -1);
     puts("poll");
+    lseek(fd, 0, SEEK_SET);
     read(fd, buf, 100);
     buf[99] = 0;
     puts(buf);
