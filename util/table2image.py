e8a708702f3d07169e91da066748fc31105fb62a
oberien <jaro.fietz@gmx.de>
2019-01-15 12:13:01 +0100

    sync, add gajim

--- util/table2image.py
+++ util/table2image.py
@@ -8,20 +8,22 @@ def dotable(table, name):
     tex = """\\documentclass[preview]{{standalone}}
     \\usepackage{{array}}
     \\renewcommand{{\\rmdefault}}{{ptm}}
-    \\newcolumntype{{C}}[1]{{>{{\\centering\\arraybackslash}}p{{#1}}}}
-    \\newcolumntype{{B}}[1]{{>{{\\raggedright\\arraybackslash}}b{{#1}}}}
+    \\newcolumntype{{C}}[1]{{>{{\\bfseries\\centering\\arraybackslash}}p{{#1}}}}
+    \\newcolumntype{{B}}[1]{{>{{\\bfseries\\raggedright\\arraybackslash}}b{{#1}}}}
+    \\usepackage[utf8]{{inputenc}}
+    \\inputencoding{{utf8}}
     \\begin{{document}}
     {}
     \\end{{document}}"""
 
     width = max(map(lambda x: len(x), table))
 
-    tabular = "\\begin{tabular}{|B{3cm}|C{5cm}|C{5cm}|}\n"
+    tabular = "\\begin{tabular}{|B{3cm}|C{5.5cm}|C{5.5cm}|}\n"
 
     for line in table:
         tabular += "\\hline\n"
         if len(line) == 1:
-            tabular += "\\multicolumn{{{}}}{{|l|}}{{{}}}\\\\\n".format(width, line[0])
+            tabular += "\\multicolumn{{{}}}{{|>{{\\bfseries}}l|}}{{{}}}\\\\\n".format(width, line[0])
         elif len(line) == width:
             tabular += " & ".join(line) + "\\\\\n"
         else:
@@ -45,6 +47,15 @@ def dotable(table, name):
     os.remove(name + ".pdf")
 
 tables = [[["test"], ["foo"], ["a", "b", "c"]], [["bar"], ["1", "2", "3"]]]
+
+# TODO: remove
+tables = []
+for i in range(1, 15):
+    table = []
+    with open("../../hochzeit/table" + str(i) + ".csv", "r") as f:
+        for l in f.readlines():
+            table.append(list(map(lambda x: x.strip(), l.split(","))))
+    tables.append(table)
 
 for (i, table) in enumerate(tables):
     dotable(table, "table" + str(i+1))
